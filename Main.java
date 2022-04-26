a8002d98c18cc68348c8251504ef33d3eee71028
Michael G. Padin <75446003+moklo0821@users.noreply.github.com>
2022-04-26 22:17:38 +0800

    commit

---
+++ Main.java
@@ -0,0 +1,36 @@
+/*
+public class Main {
+    public static void main(String[] args) {
+            int count = 10;
+
+            while (count >= 1) {
+                System.out.println(count);
+                count--;
+            }
+    }
+}
+*/
+
+
+//import java.util.Scanner;
+/*
+
+public class Main {
+    public static void main(String[] args) {
+        int sum = 0, count = 1, num;
+
+        Scanner console = new Scanner(System.in);
+        System.out.println("Enter 10 numbers: ");
+
+        while (count <= 10) {
+            num = console.nextInt();
+            sum += num;
+            count++;
+        }
+        console.close();
+        System.out.println("\n----------------------------------");
+        System.out.println("Sum of all numbers: " + sum);
+    }
+}
+*/
+
